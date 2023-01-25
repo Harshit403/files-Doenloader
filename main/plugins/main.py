55830e1f8712dfa911da01972c75569dc88f86d4
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-01-25 17:45:06 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -64,7 +64,23 @@ async def get_msg(userbot, client, sender, msg_link, edit):
         chat = int('-100' + str(msg_link.split("/")[-2]))
         try:
             msg = await userbot.get_messages(chat, msg_id)
-
+###########madharxgod
+            if msg.media:
+                if msg.media == MessageMediaType.WEB_PAGE:
+                    edit = await client.edit_message_text(sender, edit_id, "⚡")
+                    await client.send_message(sender, msg.text.markdown)
+                    await edit.delete()
+                    return
+            if not msg.media:
+                if msg.text:
+                    #edit = await client.edit_message_text(sender, edit_id, "⏳")
+                    await client.send_message(sender, msg.text.markdown)
+                    await edit.delete()
+                    return
+                if msg.empty or msg.service or msg.dice or msg.location:
+                    edit = await client.edit_message_text(sender, edit_id, "This message doesn't exist.")
+                    return 
+################madharchod
             edit = await edit.edit('Processing...')
 #end
             file = await userbot.download_media(
