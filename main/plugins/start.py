03ed0d5160d7c727024b1ae49902666d71c190a1
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-01-26 22:59:10 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -43,7 +43,7 @@ Heroku = heroku3.from_key(API_KEY)
 async def is_heroku():
     return "heroku" in socket.getfqdn()
 #emd
-st = "Hii,\nI am @pyrogrammers save restricted contents bot, I can save messages of restricted channels.\n**Hit /help to learn more.**"
+st = "Hii,\nI am @pyrogrammers save restricted contents bot, I can save messages of restricted channels.\n**Hit /help to learn more.**\n\n__You can get your own stable bot by contacing @MichaelPanther at cheap rate.__"
 #define downloads
 downloads = os.path.realpath("main/downloads")
 raw = os.path.realpath(".")
