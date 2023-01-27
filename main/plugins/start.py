23b0db9189ce66330da555b6d0bea2180c1d27f5
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-01-27 11:34:13 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -89,7 +89,7 @@ async def start(event):
         os.execl(sys.executable, sys.executable, *sys.argv)
         quit()
     else:
-        await Dick.edit(f'👋 Hi **{event.sender.first_name}**,\n\nI am Save Restricted Contents Bot, I can save files of restricted channels as well as group.\n\n__Hit /help to learn more.__\n\nGet personal stable bot from @MichaelPanther at cheap rate.__', 
+        await Dick.edit(f'👋 Hi **{event.sender.first_name}**,\n\nI am Save Restricted Contents Bot, I can save files of restricted channels as well as group.\n\n__Hit /help to learn more.__\n\n__Get your own personal stable bot from @MichaelPanther at cheap rate.__', 
                       buttons=[
                         [Button.url("📢 Updates Channel", url="https://t.me/pyrogrammers"),
                          Button.url("👥 Support Group", url="https://t.me/+e0hay-RhwP45ZjM1")],
