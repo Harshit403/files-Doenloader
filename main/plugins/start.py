bf2bfc09586f02a11466aac3c7af342c430a084b
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-02-16 19:37:37 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -43,7 +43,7 @@ Heroku = heroku3.from_key(API_KEY)
 async def is_heroku():
     return "heroku" in socket.getfqdn()
 #emd
-st = "Hii,\nI am @pyrogrammers save restricted contents bot, I can save messages of restricted channels.\n**Hit /help to learn more.**\n\n__You can get your own stable bot by contacing @MichaelPanther at cheap rate.__"
+st = "Hii,\nI am @pyrogrammers save restricted contents bot, I can save messages of restricted channels.\n**Hit /help to learn more.**\n\n__You can get your own stable bot by contacing @Mich.__"
 #define downloads
 downloads = os.path.realpath("main/downloads")
 raw = os.path.realpath(".")
@@ -72,7 +72,7 @@ async def start(event):
     if time_taken_s >= 700:
         #await event.answer("🔴 Bot Restarted due to high ping value.")
         #await event.send_message(LOGS, "#restart Restarting... It will take upto 5 seconds.")
-        await Dick.edit(f'👋 Hey **{event.sender.first_name}**,\n\nI am Save Restricted Contents Bot, I can save files of restricted channels as well as group.\n\n__Hit /help to learn more.__\n\n__Get personal stable bot from @MichaelPanther at cheap rate.__', 
+        await Dick.edit(f'👋 Hey **{event.sender.first_name}**,\n\nI am Save Restricted Contents Bot, I can save files of restricted channels as well as group.\n\n__Hit /help to learn more.__', 
                       buttons=[
                         [Button.url("📢 Updates Channel", url="https://t.me/Pyrogrammers"),
                          Button.url("👥 Support Group", url="https://t.me/+e0hay-RhwP45ZjM1")],
@@ -89,7 +89,7 @@ async def start(event):
         os.execl(sys.executable, sys.executable, *sys.argv)
         quit()
     else:
-        await Dick.edit(f'👋 Hi **{event.sender.first_name}**,\n\nI am Save Restricted Contents Bot, I can save files of restricted channels as well as group.\n\n__Hit /help to learn more.__\n\n__Get your own personal stable bot from @MichaelPanther at cheap rate.__', 
+        await Dick.edit(f'👋 Hi **{event.sender.first_name}**,\n\nI am Save Restricted Contents Bot, I can save files of restricted channels as well as group.\n\n__Hit /help to learn more.__', 
                       buttons=[
                         [Button.url("📢 Updates Channel", url="https://t.me/pyrogrammers"),
                          Button.url("👥 Support Group", url="https://t.me/+e0hay-RhwP45ZjM1")],
