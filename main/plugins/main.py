991800ec64404d7ec6f81944965b976bf1d7ed14
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-03-13 14:16:03 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -149,8 +149,11 @@ async def get_msg(userbot, client, sender, msg_link, edit):
             await edit.delete()
             await set_timer(client, sender, process, timer) 
         except Exception as e:
-            await edit.edit(F'ERROR.: {str(e)}')
-            return 
+            if '400 CHANNEL_INVALID' in str(e):
+                await edit.edit(F'You haven't joined this channel yet, please join this channel first then send me message link to save.')
+            else:
+                await edit.edit(F'ERROR: {str(e)}')
+                return 
     else:
         st, r = check_timer(sender, process, timer) 
         if st == False:
