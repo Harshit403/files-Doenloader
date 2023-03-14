363966a73b9bb2f215fcc57b6577f8df673d7316
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-03-15 01:13:57 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -214,13 +214,7 @@ async def clone(bot, event):
         return
     if not await check_user(event.chat.id):
         return await event.reply(f"You have to join my channel in order to use.\n\nDue to overload only my channel subscribers can use me.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url="https://t.me/pyrogrammers")]]),)
-    try:
-        edit = await event.reply("Initiating...")
-    except:
-        try:
-            edit = await bot.reply("Initiating...")
-        except:
-            edit = await Bot.reply("Initiating...")
+    edit = await event.reply("Initiating...")
     if 't.me' in link and not 't.me/c/' in link and not 't.me/+' in link:
         try:
             await get_msg(bot, bot, event.chat.id, link, edit)
