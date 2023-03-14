29928122ba15ff41daf7d469fd4ddf9bd765b8b4
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-03-14 23:02:54 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -208,9 +208,7 @@ async def clone(bot, event):
            return
     except TypeError:
         return
-
-    edit = await Bot.send_message(event.chat.id, "⏳")
-    
+    edit = await event.reply("Initiating...")
     if 't.me' in link and not 't.me/c/' in link and not 't.me/+' in link:
         try:
             await get_msg(bot, bot, event.chat.id, link, edit)
