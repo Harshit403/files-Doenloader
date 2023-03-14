7b64971a574bc67a010248dcad52dcfbb89b4a8d
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-03-14 23:16:07 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -2,6 +2,7 @@ from asyncio.exceptions import TimeoutError
 from pyrogram import filters, Client, idle
 from pyrogram.types import Message
 import os
+from telethon import events, Button, errors, functions
 from main.plugins.helpers import forcesub, forcesub_text
 import requests
 import heroku3
@@ -22,7 +23,7 @@ from pyrogram import Client
 import shutil, psutil
 from utils_bot import *
 #end
-from main.plugins.main import Bot
+from main.plugins.main import Bot, check_user
 from pyromod import listen 
 from main.plugins.helpers import login, logout
 from main.Database.database import Database
@@ -99,6 +100,9 @@ async def start(event):
                         [Button.inline("💲 Donate", data="cbdonate"),
                          Button.inline("🗑️ Close", data="cbclose")]
                     ])
+        if not await check_user(event.sender_id):
+            return await Dick.edit(f"Hello {event.sender.first_name}, Due to overload only my channel subscribers can use me.\n\nPlease join my channel and then start me again!", buttons=[Button.url("Join Channel", url="https://t.me/pyrogrammers")])
+
 #end message 
     tag = f'[{event.sender.first_name}](tg://user?id={event.sender_id})'
     await event.client.send_message(int(ACCESS), f'#NEW_USER {tag} started the BOT\nUserID: {event.sender_id}') 
