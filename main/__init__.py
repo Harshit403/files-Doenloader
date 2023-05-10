03e7bba8bac6a78af399097d54d9fde089640c62
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-05-10 18:02:44 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,6 +12,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
+BOT_TOKEN2 = config("BOT_TOKEN", default=None)
+BOT_TOKEN3 = config("BOT_TOKEN", default=None)
 FORCESUB = config("FORCESUB", default=None, cast=int)
 ACCESS = config("ACCESS", default=None, cast=int)
 MONGODB_URI = config("MONGODB_URI", default=None)
