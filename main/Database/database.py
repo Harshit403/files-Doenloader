5dc9cb4aea82488bdc8c3f4285662f28b7179f22
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-05-11 01:49:58 +0530

    Update database.py

--- main/Database/database.py
+++ main/Database/database.py
@@ -84,3 +84,11 @@ class Database:
     async def lout(self, id):
         await self.col.update_one({'id': id}, {'$set': {'log': False}})
 
+
+    async def cin(self, id):
+        await self.col.update_one({'id': id}, {'$set': {'log': True}})
+    
+    async def is_connected(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        banned = user.get('log', False)
+        return banned
