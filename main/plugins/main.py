405ee9a4f7f8e2bf34d245a0586efbe3c0b33257
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-05-11 20:30:16 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -201,31 +201,7 @@ async def clone(bot, event):
     edit = await Bot.send_message(event.chat.id, "⏳")
     if not await check_user(event.chat.id):
         return await edit.edit(f"Hello {event.chat.first_name}, Due to overload only my channel subscribers can use me.\n\nPlease join my channel and then start me again!", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url="https://t.me/pyrogrammers")]]),)
-  #-----------------------------------
-  #-----------------------------------
-     fuck = "" 
-     MONGODB_URI = config("MONGODB_URI", default=None) 
-     db = Database(MONGODB_URI, 'saverestricted') 
-     i, h, t = await db.get_token(event.chat.id) 
-     if i and h and t is not None: 
-         print(t)
-         try: 
-             fuck = Client( 
-                 "save-restricted-ot", 
-                  bot_token=t, 
-                  api_id=int(i), 
-                  api_hash=h
-              ) 
-             await fuck.start() 
-         except ValueError: 
-             return await edit.edit("Your bot cridentials are not valid, please /connect and /login again.") 
-         except Exception as e: 
-             print(e) 
-             return await edit.edit(f'{str(e)}') 
-     else: 
-         return await edit.edit("⚠️You are not connected.\nHit /connect to connect in to the bot.") 
-  #----------------------------------_
-  #----------------------------------_
+
 
     if 't.me' in link and not 't.me/c/' in  link and not 't.me/+' in link:
         try:
