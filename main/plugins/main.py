c11d81e9c970bdc366034483f333034324ca4e65
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-05-23 17:21:14 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -197,7 +197,10 @@ async def clone(bot, event):
     #if xx is True:
         #await event.reply('You have to join @pyrogrammers in order to use me.',reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url="https://t.me/pyrogrammers")]]),)
        # return
-    edit = await bot.send_message(event.chat.id, '⏳')
+    try:
+        edit = await event.reply('⏳')
+    except Exception as e:
+        await event.reply(f'Error: {e}')
     if not await check_user(event.chat.id):
         return await edit.edit(f"Hello {event.chat.first_name}, Due to overload only my channel subscribers can use me.\n\nPlease join my channel and then start me again!", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url="https://t.me/pyrogrammers")]]),)
 
