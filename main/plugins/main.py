6dcf46fd8e25d5fbbbfc121d614bcf422b7a2c97
Harshit <hshrivastav100@gmail.com>
2025-10-04 13:48:46 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -1,312 +1,155 @@
-import os, time, asyncio, \
-    requests, shutil, random, logging
+import os, time, asyncio, requests, shutil, random, logging, re, cv2
 from pyrogram.enums import MessageMediaType
-from .. import bot as Drone, bot
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from .. import bot, API_ID, API_HASH, BOT_TOKEN, FORCESUB, ACCESS, FORCESUB, Bot
-import os
-from pyrogram.errors import ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid
-from main.plugins.helpers import get_link, join, set_timer, check_timer, screenshot
-from main.plugins.display_progress import progress_for_pyrogram
-from main.Database.database import Database
-from decouple import config
-from telethon import Button
+from pyrogram.errors import FloodWait, BadRequest
+from pyrogram import Client, filters
 from telethon import events, Button
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.errors.rpcerrorlist import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
-from pyrogram.errors import FloodWait, BadRequest
-from pyrogram import Client, filters, idle
-import re, time, asyncio, logging
-from PyHarshit.tg.extractor import videoMetaData
-from PyHarshit.tg.Control import uploadFile
+from .. import bot, API_ID, API_HASH, BOT_TOKEN, FORCESUB, ACCESS, Bot
+from ..plugins.helpers import get_link, join, set_timer, check_timer
+from ..plugins.display_progress import progress_for_pyrogram
+from ..Database.database import Database
+from main.plugins.dbstuff import db
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-process=[]
-timer=[]
+process = []
+timer = []
 
-async def check_user(id):
-    ok = True
+async def is_user_subscribed(uid):
+    if not FORCESUB: return True
     try:
-        await bot(GetParticipantRequest(channel=FORCESUB, participant=id))
-        ok = True
+        await bot(GetParticipantRequest(channel=FORCESUB, participant=uid))
+        return True
     except UserNotParticipantError:
-        ok = False
-    return ok
+        return False
+
+def video_meta(path):
+    cap = cv2.VideoCapture(path)
+    if not cap.isOpened(): return dict(duration=0, width=0, height=0)
+    width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps    = cap.get(cv2.CAP_PROP_FPS) or 25
+    frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    duration = int(frames / fps) if fps else 0
+    cap.release()
+    return dict(duration=duration, width=width, height=height)
+
+async def gen_thumb(video, out):
+    cap = cv2.VideoCapture(video)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, int(cap.get(cv2.CAP_PROP_FRAME_COUNT) // 2))
+    ret, frame = cap.read()
+    cap.release()
+    if ret:
+        cv2.imwrite(out, frame)
+        return out
+    return None
 
 async def get_msg(userbot, client, sender, msg_link, edit):
-    chat = "" 
-    round_message = False 
-    msg_id = 0
-    height, width, duration, thumb_path = 90, 90, 0, None
-    try:
-        await client.get_chat(sender)
-    except:
-        me = await client.get_me()
-        await edit.edit(f"👀 You need to start a chat with @{me.username} first.")
-        return None
-    try:
-        msg_id = int(msg_link.split("/")[-1])
-    except ValueError:
-        if '?single' in msg_link:
-            link_ = msg_link.split("?single")[0]
-            msg_id = int(link_.split("/")[-1])
-        else:
-            await edit.edit(f"🚫 Seems like you sent an unsupported link.")
-            return None
-    if 't.me/c/' in msg_link:
-        st, r = check_timer(sender, process, timer) 
-        if st == False:
-            await Bot.send_message(sender, r) 
-        if 't.me/b/' in msg_link:
-            chat = str(msg_link.split("/")[-2])
-        else:
-            chat = int('-100' + str(msg_link.split("/")[-2]))
-        file = ""
-        try:
-            try:
-                msg = await userbot.get_messages(chat, msg_id)
-            except FloodWait as f:
-                await edit.edit(f'Bot is sleeping for {f.value} seconds due to telegram limitations.')
-            except Exception as e:
-                await edit.edit(e)
-            if msg.media: 
-                if msg.media == MessageMediaType.WEB_PAGE:
-                    edit = await edit.edit('⏳')
-                    await client.send_message(sender, msg.text.markdown)
-                    me = await client.get_me()
-                    await edit.edit(f'Your file has been forwarded to @{me.username}.')
-                    #await set_timer(Bot, sender, process, timer)
-                    return
-            if not msg.media:
-                if msg.text:
-                    edit = await edit.edit("Forwarding...")
-                    await client.send_message(sender, msg.text.markdown)
-                    me = await client.get_me()
-                    await edit.edit(f'Your file has been forwarded to @{me.username}.')
-                    #await set_timer(Bot, sender, process, timer)
-                    return
-            edit = await edit.edit('Processing...')
-            file = await userbot.download_media(
-                msg,
-                progress=progress_for_pyrogram,
-                progress_args=(
-                    userbot,
-                    "🟢 Downloading:\n",
-                    edit,
-                    time.time()
-                )
-            )
-            await edit.edit('⚪ Uploading...')
-            caption = None
-            if msg.caption is not None:
-                caption = msg.caption
-            caption_entities = msg.caption_entities 
-            if msg.media==MessageMediaType.VIDEO_NOTE: 
-                 round_message = True 
-                 print("Trying to get metadata") 
-                 data = videoMetaData(file) 
-                 height, width, duration = data["height"], data["width"], data["duration"] 
-                 print(f'd: {duration}, w: {width}, h:{height}') 
-                 try: 
-                     thumb_path = await screenshot(file, duration, sender) 
-                 except Exception: 
-                     thumb_path = None 
-                 await client.send_video_note( 
-                     chat_id=sender, 
-                     video_note=file, 
-                     length=height, duration=duration,  
-                     thumb=thumb_path, 
-                     progress=progress_for_pyrogram, 
-                     progress_args=( 
-                         client, 
-                         'Uploading...\n', 
-                         edit, 
-                         time.time() 
-                     ) 
-                 )
-                 me = await client.get_me()
-                 await edit.edit(f'Your file has been forwarded to @{me.username}.')
-            #await set_timer(Bot, sender, process, timer)
-            elif msg.media==MessageMediaType.VIDEO and msg.video.mime_type in ["video/mp4", "video/x-matroska"]:
-                if str(file).split(".")[-1] in ['webm', 'mkv']:
-                    path = str(file).split(".")[0] + ".mp4"
-                    os.rename(file, path) 
-                    file = str(file).split(".")[0] + ".mp4"
-                data = videoMetaData(file)
-                height, width, duration = data["height"], data["width"], data["duration"]
-                print(f'd: {duration}, w: {width}, h:{height}')
-                thumb_path = await screenshot(file, duration/2, sender)
-                await client.send_video(
-                    chat_id=sender,
-                    video=file,
-                    caption=caption,
-                    caption_entities=caption_entities,
-                    supports_streaming=True,
-                    duration=duration,
-                    thumb=thumb_path,
-                    progress=progress_for_pyrogram,
-                    progress_args=(
-                        client,
-                        '**Uploading:**\n',
-                        edit,
-                        time.time()
-                    )
-                )
-                me = await client.get_me()
-                try:
-                    await edit.edit(f'Your file has been Forwarded to @{me.username}.')
-                except:
-                    #pass
-                    await Bot.send_message(sender, 'Your file has been forwarded to @{me.username}.')
-                #await edit.edit(f'Your file has been forwarded to @{me.username}.')
-                #await set_timer(Bot, sender, process, timer)
-            elif str(file).split(".")[-1] in ['jpg', 'jpeg', 'png', 'webp']:
-                await edit.edit("Uploading image file...")
-                await client.send_photo(sender, file, caption=caption)
-                me = await client.get_me()
-                await edit.edit(f'Your file has been forwarded to @{me.username}.')
-                #await set_timer(Bot, sender, process, timer)
-            elif str(file).split(".")[-1] in ['mp3', 'ogg', 'wav', 'm4a', 'Flac', 'AAC']:
-                await edit.edit("Uploading Audio File...")
-                await client.send_audio(sender, file, caption=caption)
-                me = await client.get_me()
-                await edit.edit(f'Your file has been forwarded to @{me.username}.') 
-                #await set_timer(Bot, sender, process, timer)
-            else:
-                await client.send_document(
-                    sender,
-                    file, 
-                    caption=caption,
-                    progress=progress_for_pyrogram,
-                    progress_args=(
-                        client,
-                        'Uploading...\n',
-                        edit,
-                        time.time()
-                    )
-                )
-            me = await client.get_me()
-            await edit.edit(f'Your file has been forwarded to @{me.username}.')
-            #await set_timer(Bot, sender, process, timer)
-            try:
-                os.remove(file)
-                if os.path.isfile(file) == True:
-                    os.remove(file)
-            except Exception:
-                pass
-            await edit.delete()
-        except (ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid):
-            await edit.edit("There is a problem in your source channel.")
-            return
-        except Exception as e:
-            await edit.edit(e)
-            return
-            
+    if 't.me/c/' in msg_link or 't.me/b/' in msg_link:
+        chat_id = int('-100' + msg_link.split('/')[-2]) if 't.me/c/' in msg_link else int(msg_link.split('/')[-2])
+    elif 't.me/' in msg_link:
+        chat_id = msg_link.split('/')[-2]
     else:
-        st, r = check_timer(sender, process, timer) 
-        if st == False:
-            await Bot.send_message(sender, r)
-            return await edit.delete()
-        chat =  msg_link.split("/")[-2]
-        try:
-            await client.copy_message(int(sender), chat, msg_id)
-            me = await client.get_me()
-            await edit.edit(f'Your file has been forwarded to @{me.username}.')
-            await set_timer(Bot, sender, process, timer)
-        except FloodWait as f: 
-            return await edit.edit(f"Bot is limited by telegram for {f.value + 2} seconds.")
-            await asyncio.sleep(f.value)
-        except Exception as e:
-            if "Empty messages cannot be copied" in str(e):
-                group = await userbot.get_users(chat)
-                group_link = f't.me/c/{int(group.id)}/{int(msg_id)}'
-                return await get_msg(userbot, JVbot, bot, sender, edit_id, msg_link, i)
-            else:
-                print(e)
-                return await edit.edit(sender, f'{str(e)}')
-        except (ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid):
-            await client.edit_message_text(sender, edit_id, "Send Invite Link First.")
-        
-@Bot.on_message(filters.private & filters.incoming)
-async def clone(bot, event):
-    try:
-       link = get_link(event.text)
-       if not link:
-           return
-    except TypeError:
+        await edit.edit('Unsupported link.')
         return
-    try:
-        edit = await event.reply('⏳')
-    except Exception as e:
-        await event.reply(f'Error: {e}')
-    if not await check_user(event.chat.id):
-        return await edit.edit(f"Hello {event.chat.first_name}, Due to overload only my channel subscribers can use me.\n\nPlease join my channel and then start me again!", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url=f"https://t.me/{FORCESUB}")]]),)
-    MONGODB_URI = config("MONGODB_URI", default=None)
-    db = Database(MONGODB_URI, 'saverestricted')
-    bot_token = await db.get_botCreds(event.chat.id)
-    if bot_token:
-        try:
-            JVbot = Client(
-                name=str(bot_token.split(":")[0]),
-                api_hash=API_HASH,
-                api_id=API_ID,
-                bot_token=bot_token,
-                in_memory=True)
-            await JVbot.start()
-        except ValueError:
-            return await edit.edit("Your bot token is not valid, please /disconnect and /connect again.")
-        except Exception as e:
-            print(e)
-            return await edit.edit(f'{str(e)}')
-    else:
-        return await edit.edit("⚠️Your bot is not connected yet.\nHit /connect to connect the bot.")
-    if 't.me' in link and not 't.me/c/' in link and not 't.me/+' in link:
-        try:
-            await get_msg(bot, JVbot, event.chat.id, link, edit)
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-        except ValueError as v:
-            return await edit.edit(f'`{str(v)}`')
-            await asyncio.sleep(2)
-        except Exception as e:
-            return await edit.edit(f'Error: `{str(e)}`')   
-            await asyncio.sleep(2)      
-        except FloodWait as e:
-            return await edit.edit(f"Bot is limited by telegram for {e.value + 2} seconds.")
+    msg_id = int(msg_link.split('/')[-1].split('?')[0])
 
-    userbot = ""
-    i, h, s = await db.get_credentials(event.chat.id)
-    if i and h and s is not None:
-        try:
-            userbot = Client(
-                name=s[0:15],
-                session_string=s,
-                api_hash=h,
-                api_id=int(i),
-                in_memory=True)
-            await userbot.start()
-        except ValueError:
-            return await edit.edit("Your login cridentials are not valid, please /logout and /login again.")
-        except Exception as e:
-            print(e)
-            return await edit.edit(f'{str(e)}')
+    try:
+        msg = await userbot.get_messages(chat_id, msg_id)
+    except Exception as e:
+        await edit.edit(f'Failed to fetch: {e}')
+        return
+
+    if not msg.media:
+        await client.send_message(sender, msg.text or msg.caption or '')
+        me = await client.get_me()
+        await edit.edit(f'Forwarded to @{me.username}.')
+        return
+
+    file = await userbot.download_media(
+        msg,
+        progress=progress_for_pyrogram,
+        progress_args=(userbot, 'Downloading…', edit, time.time())
+    )
+    caption = msg.caption or ''
+    entities = msg.caption_entities
+
+    await edit.edit('Uploading…')
+
+    if msg.video_note:
+        meta = video_meta(file)
+        thumb = await gen_thumb(file, f'{sender}.jpg')
+        await client.send_video_note(sender, file, length=meta['height'], duration=meta['duration'],
+                                     thumb=thumb, progress=progress_for_pyrogram,
+                                     progress_args=(client, 'Uploading…', edit, time.time()))
+    elif msg.video:
+        meta = video_meta(file)
+        thumb = await gen_thumb(file, f'{sender}.jpg')
+        await client.send_video(sender, file, caption=caption, caption_entities=entities,
+                                duration=meta['duration'], width=meta['width'], height=meta['height'],
+                                thumb=thumb, progress=progress_for_pyrogram,
+                                progress_args=(client, 'Uploading…', edit, time.time()))
+    elif msg.photo:
+        await client.send_photo(sender, file, caption=caption, caption_entities=entities)
+    elif msg.audio:
+        await client.send_audio(sender, file, caption=caption, caption_entities=entities,
+                                progress=progress_for_pyrogram,
+                                progress_args=(client, 'Uploading…', edit, time.time()))
     else:
-        return await edit.edit("⚠️You are not logged in.\nHit /login to log in to the bot.")
+        await client.send_document(sender, file, caption=caption, caption_entities=entities,
+                                   progress=progress_for_pyrogram,
+                                   progress_args=(client, 'Uploading…', edit, time.time()))
+
+    me = await client.get_me()
+    await edit.edit(f'Forwarded to @{me.username}.')
+    if os.path.isfile(file): os.remove(file)
+    if os.path.isfile(f'{sender}.jpg'): os.remove(f'{sender}.jpg')
+
+@Bot.on_message(filters.private & filters.incoming)
+async def clone_handler(bot_, event):
+    link = get_link(event.text)
+    if not link:
+        return
+    init = await event.reply('⏳')
+    if not await is_user_subscribed(event.chat.id):
+        return await init.edit('Join the channel first.', reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton('Join', url=f'https://t.me/{FORCESUB}')]]))
+
+    bot_token = await db.get_botCreds(event.chat.id)
+    if not bot_token:
+        return await init.edit('Connect your bot via /connect.')
+
+    jvbot = Client(bot_token.split(':')[0], api_id=API_ID, api_hash=API_HASH,
+                   bot_token=bot_token, in_memory=True)
+    try:
+        await jvbot.start()
+    except Exception as e:
+        return await init.edit(f'Bot token error: {e}')
+
+    creds = await db.get_credentials(event.chat.id)
+    if not creds or not all(creds):
+        return await init.edit('Login first via /login or /session.')
+
+    userbot = Client('user', api_id=creds[0], api_hash=creds[1],
+                     session_string=creds[2], in_memory=True)
+    try:
+        await userbot.start()
+    except Exception as e:
+        return await init.edit(f'User session error: {e}')
+
     if 't.me/+' in link:
-        xy = await join(userbot, link)
-        await edit.edit(xy)
-        return 
-    if 't.me/c' in link:
+        out = await join(userbot, link)
+        await init.edit(out)
+    else:
         try:
-            await get_msg(userbot, JVbot, event.chat.id, link, edit)
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
+            await get_msg(userbot, jvbot, event.chat.id, link, init)
+        except FloodWait as fw:
+            await asyncio.sleep(fw.value)
         except Exception as e:
-            return await edit.edit(f'Error: `{str(e)}`')
-            await asyncio.sleep(2)         
-        except (ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid):
-            await edit.edit("Send Invite Link of your private chat first.")
-            await asyncio.sleep(2)
+            await init.edit(f'Error: {e}')
+    await userbot.stop()
+    await jvbot.stop()
